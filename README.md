1547d38e0ea01a883afd97a4e050354944cf5d7d
RichardLiu <liujh16@lenovo.com>
2018-10-19 19:52:15 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # SpiderData
 此项目用于使用python爬数据，并且对数据进行分析。
+分析博客的访问量
 
 ## 开发环境
 - IDE:pycharm
 - python version: 3.6
+
 
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello world')
+import re
+import ssl
+from urllib import request
+context = ssl._create_unverified_context()
+
+if __name__ == "__main__":
+    response = request.urlopen("https://blog.csdn.net/richard_liujh", context=context)
+    # 可以使用chardet.detect()来监测网页的编码格式
+    html = response.read().decode('utf-8')
+    print(html)
+    text = re.com
+    print('hello world')
