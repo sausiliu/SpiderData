bd122425242edbdbadde373c081c93f9a1942a31
RichardLiu <liujh16@lenovo.com>
2019-02-12 14:26:27 +0800

    update

--- src/main.py
+++ src/main.py
@@ -13,4 +13,19 @@ if __name__ == "__main__":
     #print(html)
     #text = re.compile()
     soup = BeautifulSoup(html, "lxml")
+    #print(soup.prettify())
     print(soup)
+    print("-------------------------------------")
+    print(soup.title)
+    #print(soup.title.name)
+    #print(soup.title.string)
+    #print(soup.prettify())
+
+    for link in soup.find_all('link'):
+        print(link.get('href'))
+
+    for p_tag in soup.find_all('p'):
+        if p_tag.has_attr('class'):
+            print(p_tag['class'])
+    print("-------------------------------------")
+
