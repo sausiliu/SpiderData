dd5e2c21af213f6392336bbd0bc591f4c673a742
RichardLiu <liujh16@lenovo.com>
2019-02-12 16:55:02 +0800

    update

--- src/main.py
+++ src/main.py
@@ -10,22 +10,27 @@ if __name__ == "__main__":
     response = request.urlopen(url, context=context)
     # 可以使用chardet.detect()来监测网页的编码格式
     html = response.read().decode('utf-8')
-    #print(html)
-    #text = re.compile()
+    # print(html)
+    # text = re.compile()
     soup = BeautifulSoup(html, "lxml")
-    #print(soup.prettify())
-    print(soup)
+    # print(soup.prettify())
+    # print(soup.body)
     print("-------------------------------------")
-    print(soup.title)
-    #print(soup.title.name)
-    #print(soup.title.string)
-    #print(soup.prettify())
+    print("dl:")
+    # print(soup.dl)
+    center = soup.find('dl', class_='text-center', id='fanBox')
+    print(center)
+    fan = int(center.find('span', id="fan").get_text())
+    print("fans:", fan)
 
-    for link in soup.find_all('link'):
-        print(link.get('href'))
+    # print(soup.title.name)
+    # print(soup.title.string)
+    # print(soup.prettify())
 
-    for p_tag in soup.find_all('p'):
-        if p_tag.has_attr('class'):
-            print(p_tag['class'])
+
+    # for p_tag in soup.find_all('p'):
+    #     if p_tag.has_attr('class'):
+    #         print(p_tag['class'])
     print("-------------------------------------")
 
+    print(soup.select('.read'))
